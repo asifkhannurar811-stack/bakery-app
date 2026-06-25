dc8c8941870b655f7034cc7d25f3bc936f70041c
asifkhannurar811-stack <asifkhannurar811@gmail.com>
2026-06-25 12:26:34 +0500

    Clean next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,6 @@ const nextConfig = {
     // ٹائپ سکرپٹ کی پڑتال بند کرنا تاکہ Vercel پر کوئی ایرر نہ آئے
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // ESLint کی پڑتال بھی بند کرنا
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
